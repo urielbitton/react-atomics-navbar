6b905682b7561e0ca489df60626b4c260b38e533
Uriel Bitton <55159607+urielbitton@users.noreply.github.com>
2021-04-20 15:31:16 -0400

    Added Navlinks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import "./styles.css";
+import { Router } from 'react-router-dom';
+import Navbar  from './Navbar'
 
 export default function App() {
+
+  const menuLinks = [
+    {name:'Home',url:'/',homepage:true}, 
+    {name:'Services',url:'/services',},
+    {name:'About',url:'/about',}, 
+    {name:'Our Team',url:'/our-team',}, 
+    {name:'Contact',url:'/contact',}, 
+  ]
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    <Router>
+      <Navbar 
+        gridWidth={1200}
+        logo="https://i.imgur.com/5ZuVzKu.png"
+        logoText="Cosior Inc."
+        logoTextSize={18}
+        logoTextWeight={500}
+        logoTextColor="#28344a"
+        logoWidth={40}
+        logoBorderRadius={10}
+        navWidth={1200}
+        menuLinks={menuLinks}
+      />
+    </Router>
+  )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import "./styles.css"
 
 export default function Navbar(props) {
-  return (
-    <div className="ran-nav">
 
+  const {
+    navWidth='100%', 
+    navHeight=100, 
+    navBg='transparent', 
+    orientation='space-between',
+    vertical=false,
+    reverse=false,
+    zIndex=900,
+    navPadding=20,
+    logo,
+    logoWidth,
+    logoBorderRadius,
+    navBoxShadow,
+    logoText,
+    logoTextColor,
+    logoTextSize,
+    logoTextWeight=500,
+    menuLinks=[]
+  } = props
+
+  const [mobWidth, setMobWidth] = useState(navWidth)
+
+  const navStyles = {
+    width: mobWidth,
+    height: navHeight,
+    background: navBg,
+    justifyContent: orientation,
+    padding: navPadding,
+    zIndex,
+    boxShadow: navBoxShadow
+  }
+  const logoStyles = {
+    width: logoWidth,
+    borderRadius: logoBorderRadius
+  }
+  const logoTextStyles = {
+    fontSize: logoTextSize,
+    color: logoTextColor,
+    fontWeight: logoTextWeight
+  }
+
+  const menulinksrow = menuLinks?.map(({name,url,homepage,sublinks}) => {
+    return <NavLink 
+      to={url}
+      exact={`${homepage}`}
+      activeClassName="ran-activemenulink"
+    >
+    {name}
+    </NavLink>
+  })
+
+  useEffect(() => {
+    window.onresize = () => {
+      if(window.innerWidth <= navWidth) {
+        setMobWidth('100%')
+      }
+      else {
+        setMobWidth(navWidth)
+      }
+    }
+  },[window.innerWidth])
+
+  return (
+    <div 
+      className={`ran-navbar ${vertical&&'vertical'} ${reverse&&'reverse'}`}
+      style={navStyles}
+    >
+      <div className="ran-logocont">
+        <img src={logo} alt="" style={logoStyles} />
+        <span style={logoTextStyles}>{logoText}</span>
+      </div>
+      <div className="ran-menu">
+        {menulinksrow}
+      </div>
     </div>
   ) 
 }
